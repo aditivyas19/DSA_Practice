0954d897370c7e08f7ef698494edece5a92f33c2
Aditi Vyas <aditivyas8602@gmail.com>
2026-07-06 09:55:58 +0530

    Add Hello World program

---
+++ Hello_world.cpp
@@ -0,0 +1,27 @@
+/*
+Program: Hello World
+
+This is my first C++ program.
+It prints "Hello, World!" on the console using the cout statement.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Printing the output
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
+
+/*
+Basic structure of a C++ program:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+*/
