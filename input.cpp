46bc33aa4e29dd69f95b3964360b7446e285bf13
Aditi Vyas <aditivyas8602@gmail.com>
2026-07-06 09:59:58 +0530

    Add program demonstrating user input

---
+++ input.cpp
@@ -0,0 +1,22 @@
+/*
+User Input
+
+This program takes the user's age as input and displays it on the screen.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+
+    // Taking input from the user
+    cout << "Enter your age: ";
+    cin >> age;
+
+    // Displaying the entered value
+    cout << "Your age is: " << age << endl;
+
+    return 0;
+}
