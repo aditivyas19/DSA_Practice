5b8e678a086dab46973eb0ca8d60bde6cd9fcd8b
Aditi Vyas <aditivyas8602@gmail.com>
2026-07-11 19:46:24 +0530

    Add program demonstrating while loop

---
+++ while.cpp
@@ -0,0 +1,21 @@
+/*
+While Loop
+
+This program uses a while loop to print numbers from 1 to 5.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 1;
+
+    // Continue printing until the condition becomes false
+    while (i <= 5) {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
