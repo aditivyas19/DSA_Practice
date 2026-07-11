cb99d38f2e81bcd8c7cdea00f7eea4b48ba93c7f
Aditi Vyas <aditivyas8602@gmail.com>
2026-07-11 19:45:29 +0530

    Add program demonstrating for loop

---
+++ for.cpp
@@ -0,0 +1,18 @@
+/*
+For Loop
+
+This program uses a for loop to print numbers from 1 to 5.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Using a for loop to print numbers from 1 to 5
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
